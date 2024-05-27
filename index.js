e3267d2b0b2346fae2c96a8062695183376b1bad
Hernan Rotellini <hernanrotellini@hotmail.com>
2024-05-27 19:04:31 -0300

    Update Server

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
-const port = process.env.PORT || 3001;
 const server = require("./src/app.js");
-//const { conn } = require("./src/db.js");
 
-
-  server.listen(port, () => {
-    console.log("Server funcionando puerto " + port);
-  });
+server.listen(0, function()  {
+  port = this.address().port;
+  console.log(`Server funcionando en puerto ${port}`);
+});
